c8ddc450975d8b425c0c1ae014dd566674a17207
eternalsugarzy <irwanfrozen@gmail.com>
2024-11-21 14:55:40 +0800

    menambahkan eksport data txt

--- src/view/StokView.java
+++ src/view/StokView.java
@@ -13,12 +13,21 @@ import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 import java.util.ArrayList;
 import java.text.DecimalFormat;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
 
 public class StokView extends javax.swing.JFrame {
 
     private BarangMasukController barangMasukController;
     private BarangKeluarController barangKeluarController;
     private DefaultTableModel model;
+    
 
     /**
      * Creates new form StokView
@@ -86,6 +95,45 @@ public class StokView extends javax.swing.JFrame {
         }
     }
 
+    private void exportToTXT() {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setDialogTitle("Simpan File TXT");
+
+        int userSelection = fileChooser.showSaveDialog(this);
+        if (userSelection == JFileChooser.APPROVE_OPTION) {
+            File fileToSave = fileChooser.getSelectedFile();
+            String filePath = fileToSave.getAbsolutePath();
+
+            if (!filePath.endsWith(".txt")) {
+                filePath += ".txt";
+            }
+
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+                // Ambil model dari tabel
+                DefaultTableModel model = (DefaultTableModel) tblStok.getModel();
+
+                // Tulis header kolom
+                writer.write("Nama Barang\tKategori\tJumlah\tHarga\tTanggal\tKeterangan");
+                writer.newLine();
+
+                // Iterasi melalui data di tabel
+                for (int i = 0; i < model.getRowCount(); i++) {
+                    for (int j = 0; j < model.getColumnCount(); j++) {
+                        writer.write(model.getValueAt(i, j).toString());
+                        if (j < model.getColumnCount() - 1) {
+                            writer.write("\t"); // Pisahkan kolom dengan tab
+                        }
+                    }
+                    writer.newLine(); // Pindah ke baris baru
+                }
+
+                JOptionPane.showMessageDialog(this, "Data berhasil diekspor ke " + filePath);
+            } catch (IOException e) {
+                JOptionPane.showMessageDialog(this, "Gagal mengekspor data: " + e.getMessage());
+            }
+        }
+    }
+
     private void btnKeluarActionPerformed(java.awt.event.ActionEvent evt) {
         this.dispose(); // Menutup jendela saat ini
         MainView.closeInstance(); // Tutup instance yang ada
@@ -107,6 +155,7 @@ public class StokView extends javax.swing.JFrame {
         btnKeluar = new javax.swing.JButton();
         txtSearch = new javax.swing.JTextField();
         btnSearch = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -134,6 +183,13 @@ public class StokView extends javax.swing.JFrame {
             }
         });
 
+        jButton1.setText("Eksport Data");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -144,6 +200,8 @@ public class StokView extends javax.swing.JFrame {
                 .addGap(27, 27, 27)
                 .addComponent(btnSearch)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addGap(26, 26, 26)
                 .addComponent(btnKeluar)
                 .addContainerGap())
         );
@@ -156,7 +214,8 @@ public class StokView extends javax.swing.JFrame {
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btnKeluar)
                     .addComponent(txtSearch, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btnSearch))
+                    .addComponent(btnSearch)
+                    .addComponent(jButton1))
                 .addContainerGap(95, Short.MAX_VALUE))
         );
 
@@ -203,6 +262,10 @@ public class StokView extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_btnSearchActionPerformed
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        exportToTXT();
+    }//GEN-LAST:event_jButton1ActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -241,6 +304,7 @@ public class StokView extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnKeluar;
     private javax.swing.JButton btnSearch;
+    private javax.swing.JButton jButton1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable tblStok;
